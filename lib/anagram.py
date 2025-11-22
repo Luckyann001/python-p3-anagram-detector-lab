723607363b4b2f2a3137feb43c3b0f4da6bcdb72
Luckyann Kagendo <kagendoluckyann@gmail.com>
2025-11-22 12:16:15 +0300

    updated pyfile

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+
+        matches = []
+        for w in word_list:
+            lw = w.lower()
+
+            # Exclude identical words
+            if lw == self.word:
+                continue
+
+            # Compare sorted letters
+            if sorted(lw) == sorted_word:
+                matches.append(w)
+
+        return matches
